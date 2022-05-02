6f3a024d52ad4afa9e932b7a21113d595a71c9dd
Fernando Malveira Aragão <fernandoarag@gmail.com>
2022-05-02 19:17:39 -0300

    Improving CREATE for Chamado

--- src/main/java/com/fernando/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/fernando/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.fernando.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fernando.helpdesk.domain.Chamado;
 
@@ -10,16 +12,31 @@ public class ChamadoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+
+	@NotNull(message = "O Campo PRIORIDADE é requerido!")
 	private Integer prioridade;
+
+	@NotNull(message = "O Campo STATUS é requerido!")
 	private Integer status;
+
+	@NotNull(message = "O Campo TITULO é requerido!")
 	private String titulo;
+
+	@NotNull(message = "O Campo OBSERVAÇÕES é requerido!")
 	private String observacoes;
+
+	@NotNull(message = "O Campo TÉCNICO é requerido!")
 	private Integer tecnico;
+
+	@NotNull(message = "O Campo CLIENTE é requerido!")
 	private Integer cliente;
+
 	private String nomeTecnico;
 	private String nomeCliente;
 
--- src/main/java/com/fernando/helpdesk/services/ChamadoService.java
+++ src/main/java/com/fernando/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.fernando.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernando.helpdesk.domain.Chamado;
+import com.fernando.helpdesk.domain.Cliente;
+import com.fernando.helpdesk.domain.Tecnico;
+import com.fernando.helpdesk.domain.dtos.ChamadoDTO;
+import com.fernando.helpdesk.domain.enums.Prioridade;
+import com.fernando.helpdesk.domain.enums.Status;
 import com.fernando.helpdesk.repositories.ChamadoRepository;
 import com.fernando.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -14,14 +21,41 @@ import com.fernando.helpdesk.services.exceptions.ObjectnotFoundException;
 public class ChamadoService {
 
 	@Autowired
-	private ChamadoRepository repository;
+	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 
 	public Chamado findById(Integer id) {
-		Optional<Chamado> obj = repository.findById(id);
+		Optional<Chamado> obj = chamadoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
 	}
 
 	public List<Chamado> findAll() {
-		return repository.findAll();
+		return chamadoRepository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return chamadoRepository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+
+		Chamado chamado = new Chamado();
+
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
