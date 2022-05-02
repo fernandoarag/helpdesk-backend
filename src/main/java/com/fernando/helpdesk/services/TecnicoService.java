e469c2155c5aa22e2c44a5b398e2b04f9ace3457
Fernando Malveira Aragão <fernandoarag@gmail.com>
2022-05-02 12:55:30 -0300

    Exceção para objecto não encontrado

--- src/main/java/com/fernando/helpdesk/services/TecnicoService.java
+++ src/main/java/com/fernando/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fernando.helpdesk.domain.Tecnico;
 import com.fernando.helpdesk.repositories.TecnicoRepository;
+import com.fernando.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! id: " + id));
 	}
 }
